18a5ef1560621f9370b8821f746c10c9fe56a778
Mohammed Terry-Jack <mohammed@wluper.com>
2023-11-19 01:29:35 +0000

    annotator updated

--- tools/annotator.py
+++ tools/annotator.py
@@ -27,21 +27,34 @@ if __name__ == "__main__":
         annotation_canvas = loadtxt(annotation_path, dtype=int)
     else:
         annotation_canvas = zeros_like(ca.evolution())
+    pen_down = False
+    prev_coords = []
+
+    def toggle_pen(event) -> None:
+        global pen_down
+        global prev_coords
+        pen_down = not pen_down
+        prev_coords = []
 
     def toggle_coordinate(event) -> None:
-        if event.xdata and event.ydata:
+        global prev_coords
+        global pen_down
+        if event.xdata and event.ydata and pen_down:
             relative_x = event.xdata/100
             relative_y = event.ydata/50
             x = int(metadata['width'] * relative_x)
             y = int(metadata['depth'] * relative_y)
-            annotation_canvas[y][x] = int(not annotation_canvas[y][x])
-            clf()
-            imshow(ca.evolution(), cmap='gray')
-            imshow(annotation_canvas, alpha=0.5)
-            draw()
+            if (x,y) not in prev_coords:
+                prev_coords.append((x,y))
+                annotation_canvas[y][x] = int(not annotation_canvas[y][x])
+                clf()
+                imshow(ca.evolution(), cmap='gray')
+                imshow(annotation_canvas, alpha=0.5)
+                draw()
     
     fig = figure()
-    fig.canvas.mpl_connect('button_press_event', toggle_coordinate)
+    fig.canvas.mpl_connect('button_press_event', toggle_pen)
+    fig.canvas.mpl_connect('motion_notify_event', toggle_coordinate)
     imshow(ca.evolution(), cmap="gray")
     imshow(annotation_canvas, alpha=0.5)
     show()
